53e3be89406ccc562fe4eddcdef0ca9980bb0999
Markus Leballeux <markus.leballeux@gmail.com>
2019-06-19 20:19:33 -0500

    Some more playing around.

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -4,35 +4,83 @@
 
 namespace hx {
 
-static int parseByteHalf(const char* p) {
-    if (*p >= 'A') { // ABCDEF
-        return static_cast<int>(*p - 'A');
-    } else { // 0123456789
-        return static_cast<int>(*p - '0');
-    }        
+static uint64_t ntoh64(const uint64_t& input) {
+  uint64_t rval;
+  uint8_t* data = reinterpret_cast<uint8_t*>(&rval);
+
+  data[0] = static_cast<uint8_t>(input >> 56);
+  data[1] = static_cast<uint8_t>(input >> 48);
+  data[2] = static_cast<uint8_t>(input >> 40);
+  data[3] = static_cast<uint8_t>(input >> 32);
+  data[4] = static_cast<uint8_t>(input >> 24);
+  data[5] = static_cast<uint8_t>(input >> 16);
+  data[6] = static_cast<uint8_t>(input >> 8);
+  data[7] = static_cast<uint8_t>(input >> 0);
+
+  return rval;
 }
+
+
+static int parseByteHalf(const char* p) {
+  if (*p >= 'A') { // ABCDEF
+    return static_cast<int>(*p - 'A');
+  } else { // 0123456789
+    return static_cast<int>(*p - '0');
+  }
+}
+
 
 Buffer::Buffer() : data_(nullptr), len_(0) {
 }
-    
-Buffer::~Buffer() {
-    if (data_)
-        delete data_;
-} 
 
-void Buffer::initFromHexString(const std::string& hex_str) {
-    const char* p_in = hex_str.c_str();
-    len_ = hex_str.size()/2;
-    data_ = new char[len_];
-    char* p_out = data_;
-    for (size_t i = 0; i < len_; ++i) {
-        const int high_half = parseByteHalf(p_in++);
-        const int low_half = parseByteHalf(p_in++);
-//        _INFO("%d, %d", high_half, low_half);
-        *p_out = static_cast<char>(high_half*16 + low_half);
-        ++p_out;
-    }
+
+Buffer::~Buffer() {
+  if (data_)
+    delete data_;
 }
 
+
+void Buffer::initFromHexString(const std::string& hex_str) {
+  const char* p_in = hex_str.c_str();
+  len_ = hex_str.size()/2;
+  data_ = new char[len_];
+  char* p_out = data_;
+  for (size_t i = 0; i < len_; ++i) {
+    const int high_half = parseByteHalf(p_in++);
+    const int low_half = parseByteHalf(p_in++);
+    *p_out = static_cast<char>(high_half*16 + low_half);
+    ++p_out;
+  }
+}
+
+
+void Buffer::advanceReadPtr(size_t advance_size) {
+//  ASSERT(offset_ + advance_size <= len_);
+  offset_ += advance_size;
+}
+
+
+size_t Buffer::getRemainingLength() const {
+//  ASSERT(len_ >= offset_);
+  return len_ - offset_;
+}
+
+
+uint64_t Buffer::getUInt64() const {
+  uint64_t value = *reinterpret_cast<const uint64_t*>(data_ + offset_);
+  if (network_byte_order_)
+    value = ntoh64(value);
+
+  return value;
+}
+
+
+uint32_t Buffer::getUInt32() const {
+  uint32_t value = *reinterpret_cast<const uint32_t*>(data_ + offset_);
+  if (network_byte_order_)
+    value = htonl(value);
+
+  return value;
+}
 
 } // namespace ct
--- src/Buffer.h
+++ src/Buffer.h
@@ -4,48 +4,26 @@
 
 namespace hx {
 
-static uint64_t ntoh64(const uint64_t& input) {
-    uint64_t rval;
-    uint8_t* data = reinterpret_cast<uint8_t*>(&rval);
-
-    data[0] = static_cast<uint8_t>(input >> 56);
-    data[1] = static_cast<uint8_t>(input >> 48);
-    data[2] = static_cast<uint8_t>(input >> 40);
-    data[3] = static_cast<uint8_t>(input >> 32);
-    data[4] = static_cast<uint8_t>(input >> 24);
-    data[5] = static_cast<uint8_t>(input >> 16);
-    data[6] = static_cast<uint8_t>(input >> 8);
-    data[7] = static_cast<uint8_t>(input >> 0);
-
-    return rval;
-}
-
 class Buffer {
 public:
-    Buffer();
-    
-    ~Buffer();
+  Buffer();
 
-    void initFromHexString(const std::string& hex_str);
+  ~Buffer();
 
-    uint64_t getUInt64() const {
-        uint64_t value = *reinterpret_cast<const uint64_t*>(data_ + offset_); 
-        if (network_byte_order_) {
-            return ntoh64(value);
-        } else {
-            return value;
-        }
-    }
+  void initFromHexString(const std::string& hex_str);
 
-    uint32_t getUInt32() const {
-       return *reinterpret_cast<const uint32_t*>(data_ + offset_); 
-    }
+  void advanceReadPtr(size_t advance_size);
+
+  size_t getRemainingLength() const;
+
+  uint64_t getUInt64() const;
+  uint32_t getUInt32() const;
 
 private:
-    char*       data_;
-    size_t      len_;
-    size_t      offset_ = 0;
-    bool        network_byte_order_ = false;
+  char*       data_;
+  size_t      len_;
+  size_t      offset_ = 0;
+  bool        network_byte_order_ = false;
 };
 
 } // namespace hx
