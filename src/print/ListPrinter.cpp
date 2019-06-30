39ff352a5a894c8b21ba5e75b4693a2b08b961e3
Markus Leballeux <markus.leballeux@gmail.com>
2019-06-30 16:52:30 -0500

    Improved the list printer.

--- src/print/ListPrinter.cpp
+++ src/print/ListPrinter.cpp
@@ -2,7 +2,7 @@
 
 namespace hx {
 
-static constexpr char PRINT_FORMAT[16] = "%-32s %-20s %s\n";
+static constexpr char PRINT_FORMAT[18] = "%-32s  %-20s  %s\n";
 
 
 void ListPrinter::startPrint() {
@@ -16,7 +16,9 @@ void ListPrinter::endPrint() {
 
 
 void ListPrinter::printInterpretation(const std::string& hex_data_str, const Interpretation& interpretation, const std::string& interpreted_value_str) {
-  printf(PRINT_FORMAT, hex_data_str.c_str(), Interpretation::interpretationToCstr(interpretation), interpreted_value_str.c_str());
+  const size_t len = hex_data_str.size();
+  const std::string hex_copy = (len > 32) ? (hex_data_str.substr(0, 30) + "..") : hex_data_str;
+  printf(PRINT_FORMAT, hex_copy.c_str(), Interpretation::interpretationToCstr(interpretation), interpreted_value_str.c_str());
 }
 
 } // namespace hx
